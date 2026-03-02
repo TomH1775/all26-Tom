4bec2d2bcf26295ad4bace8e5dc450bd880c9223
Joel Truher <joel@truher.org>
2026-03-02 08:17:21 -0800

    camera config now depends only on identity

--- raspberry_pi/tests/camera/distortion_test.py
+++ raspberry_pi/tests/camera/distortion_test.py
@@ -1,12 +1,17 @@
 import unittest
 
 from app.camera.distortion import Distortion
-from app.camera.model import Model
 from app.config.identity import Identity
 
 
 class IntrinsicTest(unittest.TestCase):
     def test_identity(self) -> None:
         identity: Identity = Identity.DEV
-        model: Model = Model.GS
-        Distortion(identity, model)
+        Distortion(identity)
+
+    # TODO: fix this test
+    @unittest.expectedFailure
+    def test_all_identities(self) -> None:
+        for identity in list(Identity):
+            distortion: Distortion = Distortion(identity)
+            self.assertTrue(distortion.valid())
--- raspberry_pi/tests/camera/intrinsic_test.py
+++ raspberry_pi/tests/camera/intrinsic_test.py
@@ -1,12 +1,17 @@
 import unittest
 
 from app.camera.intrinsic import Intrinsic
-from app.camera.model import Model
 from app.config.identity import Identity
 
 
 class IntrinsicTest(unittest.TestCase):
     def test_identity(self) -> None:
         identity: Identity = Identity.DEV
-        model: Model = Model.GS
-        Intrinsic(identity, model)
+        Intrinsic(identity)
+
+    # TODO: fix this test
+    @unittest.expectedFailure
+    def test_all_identities(self) -> None:
+        for identity in list(Identity):
+            intrinsic: Intrinsic = Intrinsic(identity)
+            self.assertTrue(intrinsic.valid())
